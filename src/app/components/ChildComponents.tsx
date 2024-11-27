f88ca752636faba479b66bb3ab04b0c1902279c0
imuskanbatool7 <muskanbatool355>
2024-11-27 20:20:53 +0500

    unknown

---
+++ src/app/components/ChildComponents.tsx
@@ -0,0 +1,18 @@
+
+
+import ParentComponent from "./ParentComponent"
+
+const ChildComponents = (pros: any) => {
+    console.log(pros)
+  return (
+    <div className="text-2xl pt-10  pl-10  bg-purple-100 min-h-screen font-semibold fontFamily:&apos;Times New border-4 border-purple-200">
+      <h1 className="border-2 border-white inline-block"> Name: {pros.name}</h1>
+      <br />
+      <h1 className="border-2 border-white inline-block"> Age:  {pros.age}</h1>
+      <br />
+      <h1 className="border-2 border-white inline-block"> Favorite Color: {pros.favcolor}</h1>
+    </div>
+  )
+}
+
+export default ChildComponents
