2514ac80e9c9f8dc471befa83612c7833ac6d743
imuskanbatool7 <muskanbatool355>
2024-12-01 12:07:50 +0500

    one

--- src/app/components/ChildComponents.tsx
+++ src/app/components/ChildComponents.tsx
@@ -2,15 +2,19 @@
 
 import ParentComponent from "./ParentComponent"
 
-const ChildComponents = (pros: any) => {
-    console.log(pros)
+const ChildComponents = (props:any) => {
+    console.log(props)
   return (
-    <div className="text-2xl pt-10  pl-10  bg-purple-100 min-h-screen font-semibold fontFamily:&apos;Times New border-4 border-purple-200 sm:text-2xl md:text-3xl lg:text-4xl ">
-      <h1 className="border-2 border-white inline-block"> Name: {pros.name}</h1>
+    
+    
+    <div className="text-center text-2xl pt-10  pl-10  bg-purple-100 min-h-screen font-bold font-serif border-2 border-purple-200 sm:text-2xl md:text-3xl lg:text-4xl ">
+      <div className="p-6 m-4 bg-slate-50 inline-block bg-gradient-to-r from-teal-400 via-purple-300 to-blue-200 rounded-lg">
+      <h1 > Name: {props.name}</h1>
       <br />
-      <h1 className="border-2 border-white inline-block"> Age:  {pros.age}</h1>
+      <h1> Age:  {props.age}</h1>
       <br />
-      <h1 className="border-2 border-white inline-block"> Favorite Color: {pros.favcolor}</h1>
+      <h1> Favorite Color: {props.favcolor}</h1>
+    </div>
     </div>
   )
 }
